ec6332160b46aa4edb5c73da00f3a0573b96a435
Anthony Dube <dubedeveloper@gmail.com>
2022-04-04 13:22:49 -0400

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <div className="navbar container">
+      <a href="#!">
+        e<span>gg</span>flip
+      </a>
+      <div className="nav-links"></div>
+      <a href="#!">Home</a>
+      <a href="#!">Recipes</a>
+      <a href="#!">Settings</a>
+    </div>
+  );
+}
